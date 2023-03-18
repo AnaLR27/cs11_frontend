dc060e6656bfb4c6a031397d58fec1f357504eea
Veronika <vsc1972@gmail.com>
2023-03-18 19:40:22 +0100

    [CSF-8]-company-profile done

---
+++ src/services/employer.service.js
@@ -0,0 +1,97 @@
+import { Employer } from '../models/employer model';
+
+export class EmployerService {
+    //GET: loginId (crear el usuario)
+    static async getById(userId) {
+        const token =
+            sessionStorage.getItem('token') || localStorage.getItem('token');
+
+        if (!token) {
+            return false;
+        }
+
+        //petición all backend
+        const request = await fetch('http://localhost:8000/employer' + userId, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Auth-token': token,
+            },
+        });
+        const data = await request.json();
+        return new Employer(data.data);
+    }
+    //PATCH: Update->UserId || loginID
+    static async editemployer(userId, body) {
+        //cogemos el token del local storage
+        const token =
+            sessionStorage.getItem('token') || localStorage.getItem('token');
+
+        //comprobamos si ha cogido el token
+        if (!token) {
+            return false;
+        }
+        // Peticion al backend
+        const request = await fetch('http://localhost:8000/employer' + userId, {
+            method: 'PATCH',
+            body: JSON.stringify(body),
+            headers: {
+                'Content-Type': 'application/json',
+                'Auth-token': localStorage.getItem('token'),
+            },
+        });
+        const data = await request.json();
+        return new Employer(data.data);
+    }
+
+    //POST:  Create-> UserId || loginID
+    static async newemployer(body) {
+        //cogemos el token del local storage
+        const token =
+            sessionStorage.getItem('token') || localStorage.getItem('token');
+
+        //comprobamos si ha cogido el token
+        if (!token) {
+            return false;
+        }
+        //Peticion al backend
+        const request = await fetch('http://localhost:8000/employer', {
+            method: 'POST',
+            body: JSON.stringify(body),
+            headers: {
+                'Content-Type': 'application/json',
+                'Auth-token': token,
+            },
+        });
+        const data = await request.json();
+        return new Employer(data.data);
+    }
+
+    //POST
+    static async uploadImage(file, _id) {
+        //cogemos el token del local storage
+        const token =
+            sessionStorage.getItem('token') || localStorage.getItem('token');
+
+        //comprobamos si ha cogido el token
+        if (!token) {
+            return false;
+        }
+        const formData = new FormData();
+        formData.append('file0', file);
+
+        //Peticion al backend
+        const request = await fetch(
+            'http://localhost:8000/employer' + _id + '/logo',
+            {
+                method: 'POST',
+                body: formData,
+                headers: {
+                    'Auth-token': token,
+                },
+            },
+        );
+        const data = await request.json();
+        return new Employer(data.data);
+    }
+}
