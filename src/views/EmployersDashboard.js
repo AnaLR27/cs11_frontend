9e0cfd3d42337a1d74203193670c5f11a5a6d929
IsmaelBoule <ismaelboulen@gmail.com>
2023-03-19 16:21:08 +0100

    [CSF-54] - Adding CSS

---
+++ src/views/EmployersDashboard.js
@@ -0,0 +1,7 @@
+import EmployersDashboard from "../components/EmployersDashboard/EmployerDashboard";
+
+const EmployersDashboard = () => {
+  return <EmployersDashboard />;
+};
+
+export default EmployersDashboard;
