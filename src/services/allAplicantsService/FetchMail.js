006999a4c2357179f89253b2389ee4ea9f4c19cd
m4lk1 <laughingoutloud90@gmail.com>
2023-03-20 10:51:13 +0100

    [CSF-37]-reparing-all-candidates

--- src/services/allAplicantsService/FetchMail.js
+++ src/services/allAplicantsService/FetchMail.js
@@ -1,23 +1,25 @@
-async function FetchAllUsers (email, name, refused, job, acepted) {
+import { EMPLOYER_JOBS } from "../../config/urls";
+
+async function FetchMail (email, name, refused, job, acepted) {
     
         const requestOptions = {
             method: 'POST',
             headers: { 'Content-Type': 'application/json',
-                        'auth-token': sessionStorage.getItem('token') ? sessionStorage.getItem('token') : localStorage.getItem('token') },
+                        'auth-token': sessionStorage.getItem('accessToken') ? sessionStorage.getItem('accessToken') : localStorage.getItem('token') },
             body: JSON.stringify({ email: email, name: name, job: job, acepted: acepted, refused: refused })
         };
         const getDatas = async () => {
             try{
-            const response = await fetch('http://localhost:8000/job/email', requestOptions)
+            const response = await fetch(`${EMPLOYER_JOBS}/email`, requestOptions)
             const data = await response.json();
-            console.log(data);
-            return true
+            return true;
             }catch(error){
-                return false
+            return false;
             }
         
         }
         let datas = await getDatas();
+        return datas;
     }
 
-export default FetchAllUsers;
+export default FetchMail;
